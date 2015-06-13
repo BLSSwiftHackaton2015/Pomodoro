dfb749d7e0ec79b9f7dab10134ceff2d4aa4da15
marcinmierzejewski1024 <marcinmierzejewski1024@gmail.com>
2015-06-13 14:17:59 +0200

    fix userDefaults add PMTimer

--- Pomodoro/Pomodoro/PMNSUserDefaults.swift
+++ Pomodoro/Pomodoro/PMNSUserDefaults.swift
@@ -12,6 +12,8 @@ extension NSUserDefaults {
     
     private static let PMStartTimeInterval = "PMStartTimeInterval"
     private static let PMTimeInterval = "PMTimeInterval"
+    private static let PMTimeLeft = "PMTimeLeft"
+    private static let PMTomatosCount = "PMTomatosCount"
     
     /// Start Time
     func setStartTimeInterval(ti: NSTimeInterval) {
@@ -19,8 +21,8 @@ extension NSUserDefaults {
         self.synchronize()
     }
 
-    func getStartTime() {
-        self.doubleForKey(NSUserDefaults.PMStartTimeInterval)
+    func getStartTime() ->NSTimeInterval {
+        return NSTimeInterval(self.doubleForKey(NSUserDefaults.PMStartTimeInterval))
     }
     
     func removeStartTime() {
@@ -33,11 +35,40 @@ extension NSUserDefaults {
         self.synchronize()
     }
     
-    func getTimeInterval() {
-        self.doubleForKey(NSUserDefaults.PMTimeInterval)
+    func getTimeInterval() -> NSTimeInterval {
+        return NSTimeInterval(self.doubleForKey(NSUserDefaults.PMTimeInterval))
     }
     
     func removeTimeInterval() {
         self.removeObjectForKey(NSUserDefaults.PMTimeInterval)
+    }
+    
+    /// Time left
+    func setTimeLeft(ti: NSTimeInterval) {
+        self.setDouble(ti, forKey: NSUserDefaults.PMTimeLeft)
+        self.synchronize()
+    }
+    
+    func getTimeLeft() -> NSTimeInterval {
+        return NSTimeInterval(self.doubleForKey(NSUserDefaults.PMTimeLeft))
+    }
+    
+    func removeTimeLeft() {
+        self.removeObjectForKey(NSUserDefaults.PMTimeLeft)
+    }
+    
+    /// Tomatos count
+    func setTomatosCount(count: Int) {
+        self.setInteger(count, forKey: NSUserDefaults.PMTomatosCount)
+        self.synchronize()
+    }
+    
+    func getTomatosCount() -> Int{
+    
+        return self.integerForKey(NSUserDefaults.PMTomatosCount)
+    }
+    
+    func removeTomatosCount() {
+        self.removeObjectForKey(NSUserDefaults.PMTomatosCount)
     }
 }
---
+++ Pomodoro/Pomodoro/PMTimer.swift
@@ -0,0 +1,9 @@
+//
+//  PMTimer.swift
+//  Pomodoro
+//
+//  Created by Marcin on 13/06/15.
+//  Copyright © 2015 Tomasz Szulc. All rights reserved.
+//
+
+import Foundation
