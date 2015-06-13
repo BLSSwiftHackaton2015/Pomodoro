ac89985a77b3cabf0e08dee7a6f075ef0bf11d69
Tomasz Szulc <tomasz.szulc@blstream.com>
2015-06-13 11:17:57 +0200

    Add Watch extension and extension for user defaults to store timer settings

---
+++ Pomodoro/Pomodoro/PMNSUserDefaults.swift
@@ -0,0 +1,44 @@
+//
+//  PMNSUserDefaults.swift
+//  Pomodoro
+//
+//  Created by Tomasz Szulc on 13/06/15.
+//  Copyright (c) 2015 Tomasz Szulc. All rights reserved.
+//
+
+import Foundation
+
+extension NSUserDefaults {
+    
+    static let PMStartTimeInterval = "PMStartTimeInterval"
+    static let PMTimeInterval = "PMTimeInterval"
+    
+    
+    /// Start Time
+    func setStartTimeInterval(ti: NSTimeInterval) {
+        self.setDouble(ti, forKey: NSUserDefaults.PMStartTimeInterval)
+        self.synchronize()
+    }
+    
+    func getStartTime() {
+        self.doubleForKey(NSUserDefaults.PMStartTimeInterval)
+    }
+    
+    func removeStartTime() {
+        self.removeObjectForKey(NSUserDefaults.PMStartTimeInterval)
+    }
+    
+    /// Time Interval
+    func setTimeInterval(ti: NSTimeInterval) {
+        self.setDouble(ti, forKey: NSUserDefaults.PMTimeInterval)
+        self.synchronize()
+    }
+    
+    func getTimeInterval() {
+        self.doubleForKey(NSUserDefaults.PMTimeInterval)
+    }
+    
+    func removeTimeInterval() {
+        self.removeObjectForKey(NSUserDefaults.PMTimeInterval)
+    }
+}
